6cbc5a596a6f17f5fc4e1e612a3f8ffbec5260df
yukiko_bass <springcomesagain129@gmail.com>
2013-04-19 14:56:17 +0900

    縦横合わせ(途中)

--- BMI/ViewController.m
+++ BMI/ViewController.m
@@ -1,15 +1,16 @@
 //
 //  ViewController.m
-//  BMI
 //
-//  Created by CatenaRentalSystem on 13/04/09.
-//  Copyright (c) 2013年 CatenaRentalSystem. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *weightText;
+@property (weak, nonatomic) IBOutlet UITextField *heightText;
+@property (weak, nonatomic) IBOutlet UILabel     *resultLabel;
+@property (strong, nonatomic) AVAudioPlayer      *audioPlayer;
 @end
 
 @implementation ViewController
@@ -17,13 +18,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (void) sound:(NSString *)file
+{
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                         pathForResource:file ofType:@"aif"]];
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    [_audioPlayer play];
+}
+
+- (IBAction)bmiButtonPushed:(id)sender
+{
+    float h = _heightText.text.floatValue / 100;
+    float w = _weightText.text.floatValue;
+    
+    float bmi = w / (h * h);
+    NSLog(@"bmi = %f", bmi);
+    
+    if (bmi >= 25) {
+        _resultLabel.text = @"太りすぎ";
+        _resultLabel.backgroundColor = [UIColor redColor];
+        [self sound:@"fat"];
+    } else if (bmi < 18.5) {
+        _resultLabel.text = @"やせすぎ";
+        _resultLabel.backgroundColor = [UIColor yellowColor];
+        [self sound:@"skinny"];
+    } else {
+        _resultLabel.text = @"標準";
+        _resultLabel.backgroundColor = [UIColor greenColor];
+        [self sound:@"normal"];
+    }
+}
 @end
